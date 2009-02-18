e7d7f8aec6646b74f97039a40f6f6210c05a4709
Liu Kang-min <gugod@gugod.org>
2009-02-18 19:30:54 +0800

    create the file for classes.

---
+++ lib/Railsish/Bootstrap.pm
@@ -0,0 +1,13 @@
+package Railsish::Bootstrap;
+# ABSTRACT: Wuu huu huu
+
+
+
+1;
+
+=head1 DESCRIPTION
+
+This class reads application configurations.
+
+=cut
+
---
+++ lib/Railsish/Dispatcher.pm
@@ -0,0 +1,12 @@
+package Railsish::Dispatcher;
+# ABSTRACT: The first handler for requests.
+
+
+1;
+
+=head1 DESCRIPTION
+
+This class contains the first handler for requests.
+
+=cut
+
