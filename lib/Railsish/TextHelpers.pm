96455a072e8555dac7bffc1205a3633062bc6f02
Kang-min Liu <gugod@gugod.org>
2009-03-23 02:51:29 +0800

    Implements all text helpers

--- lib/Railsish/TextHelpers.pm
+++ lib/Railsish/TextHelpers.pm
@@ -3,13 +3,29 @@ use strict;
 use warnings;
 use Exporter::Lite;
 
-our @EXPORT = qw(pluralize singularize);
+our @EXPORT = qw( pluralize singularize camelize camelcase underscore dasherize forien_key );
 
 use Lingua::EN::Inflect::Number qw(to_S to_PL);
 
 sub pluralize { &to_PL }
 sub singularize { &to_S }
 
+use String::CamelCase qw(camelize decamelize);
+
+sub camelcase { &camelize }
+sub underscore { &decamelize }
+
+sub dasherize {
+  my $str = &decamelize;
+  $str =~ s/_/-/g;
+  return $str;
+}
+
+sub forien_key {
+  my $str = &decamelize;
+  $str =~ s/(?!_id)$/_id/;
+  return $str;
+}
 
 1;
 
