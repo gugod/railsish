d9a1708dea0608de780b5464930d430c31e3dc89
Kang-min Liu <gugod@gugod.org>
2009-03-25 21:14:09 +0900

    controllers can now be placed under app/controllers.

--- lib/Railsish/Bootstrap.pm
+++ lib/Railsish/Bootstrap.pm
@@ -4,10 +4,24 @@ package Railsish::Bootstrap;
 use strict;
 use warnings;
 use Railsish::CoreHelpers;
+use File::Spec::Functions;
+
+sub import {
+    unshift @INC, catdir(app_root, "app", "controllers");
+}
 
 sub read_config_route {
     # XXX: is it to oldie to use .pl for config :-)
     my $config = app_root(config => "route.pl");
+}
+
+sub load_controllers {
+    my $app_root = app_root;
+    my @controllers = glob("\Q${app_root}\E/app/controllers/*Controller.pm");
+    for(@controllers) {
+	warn $_ . "\n";
+	require $_;
+    }
 }
 
 1;
