4d93e840fe1002d06b077138fd987da94373cd68
shelling <navyblueshellingford@gmail.com>
2009-03-27 22:52:16 +0800

    create controller in SimpleApp

---
+++ t/SimpleApp/app/controllers/application_controller.pm
@@ -0,0 +1,9 @@
+package SimpleApp::ApplicationController;
+use base Railish::Controller;
+
+use strict;
+use warnings;
+
+
+
+1;
---
+++ t/SimpleApp/app/controllers/index_controller.pm
@@ -0,0 +1,11 @@
+package SimpleApp::IndexController;
+use base SimpleApp::ApplicationController;
+
+use warnings;
+use strict;
+
+sub list {
+
+}
+
+1;
